41f8152c8d16e74da908f2fc389c0b07355ee825
Mansi Singh <manssingh@linkedin.com>
2025-04-03 19:21:33 +0530

    Add support for Android 16
    - Enabled edge-to-edge for FeedbackActivity and sample app.

--- shaky/src/main/java/com/linkedin/android/shaky/FeedbackActivity.java
+++ shaky/src/main/java/com/linkedin/android/shaky/FeedbackActivity.java
@@ -21,15 +21,20 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.MenuRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.annotation.StyleRes;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.annotation.MenuRes;
 
 /**
  * The main activity used capture and send feedback.
@@ -74,9 +79,16 @@ public class FeedbackActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setTheme(R.style.ShakyBaseTheme);
-
+        EdgeToEdge.enable(this);
+        ViewCompat.setOnApplyWindowInsetsListener(
+            getWindow().findViewById(android.R.id.content),
+            (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return WindowInsetsCompat.CONSUMED;
+            }
+        );
         setContentView(R.layout.shaky_feedback);
 
         customTheme = getIntent().getIntExtra(THEME, MISSING_RESOURCE);
